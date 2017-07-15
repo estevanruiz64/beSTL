6750f79b7c902265ecfa0cf2af67905fe1a04965
Estevan Ruiz <econ.eruiz@gmail.com>
2017-07-15 13:50:44 -0500

    First Commit

---
+++ src/main/java/org/launchcode/BeSTL/BeStlApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.BeSTL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeStlApplication.class, args);
+	}
+}
---
+++ src/main/java/org/launchcode/BeSTL/controllers/MainController.java
@@ -0,0 +1,25 @@
+package org.launchcode.BeSTL.controllers;
+
+import org.launchcode.BeSTL.models.CategoryListings;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+
+/**
+ * Created by estel on 7/15/2017.
+ */
+@Controller
+@RequestMapping(value="table")
+public class MainController {
+
+    HashMap<String, Integer> placeAndScore = CategoryListings.loadData();
+
+    @RequestMapping(value="")
+    public String index(Model model) {
+        model.addAttribute("listings", placeAndScore);
+        return "index";
+    }
+
+}
